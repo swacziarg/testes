3aebf35b7427429198b3d3df7f938c6999aea144
Simon Wacziarg <swacziarg@gmail.com>
2022-08-26 08:26:17 -0700

    Commiting like a comitte we getting litty

---
+++ src/test.java
@@ -0,0 +1,10 @@
+
+
+public class test {
+	public static void main (String[]args) {
+		System.out.print("Hello World");
+		System.out.print("weewoow");
+		//prints "Hi"
+		System.out.print("Matthew Ko");
+	}
+}
